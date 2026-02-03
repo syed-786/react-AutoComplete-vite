54e8cb819584235fb8d2a94735a5dc2dce6fcf9f
Syed Adeeb Ahmad <syed.adeeb786@gmail.com>
2026-02-03 23:40:58 +0400

    initial commit for AutoComplete Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import AutoCompleteInput from './components/AutoCompleteInput';
+
+function App() {
+  return (
+    <>
+      <AutoCompleteInput />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AutoCompleteInput.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import './AutoCompleteInput.css';
+
+export default function AutoCompleteInput() {
+  const [searchText, setSearchText] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const [cachedResults, setCachedResults] = useState({});
+
+  const fetchRecipes = async () => {
+    if (searchText.trim() === '') {
+      setRecipes([]);
+      return;
+    }
+
+    if (cachedResults[searchText]) {
+      console.log('Fetching from cache', cachedResults);
+      setRecipes(cachedResults[searchText]);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://dummyjson.com/recipes/search?q=${searchText}`
+      );
+      const result = await response.json();
+      setRecipes(result?.recipes);
+      console.log('Fetching from API');
+      setCachedResults(prev => ({ ...prev, [searchText]: result?.recipes }));
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      fetchRecipes();
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [searchText]);
+
+  return (
+    <div className="main-container">
+      <h1>Auto Complete Search</h1>
+      <input
+        type="text"
+        className="search-input"
+        value={searchText}
+        onChange={e => setSearchText(e.target.value)}
+        onFocus={() => setShowResults(true)}
+        onBlur={() => setTimeout(() => setShowResults(false), 100)}
+      />
+      {showResults && recipes.length > 0 && (
+        <div className="results-container">
+          {recipes?.map(item => (
+            <div className="results-name" key={item.id}>
+              {item.name}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
